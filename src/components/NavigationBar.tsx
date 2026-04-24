17660ecf186a1c015112f11339407e040ab92d2a
writepoems <272058035+writepoems@users.noreply.github.com>
2026-04-24 11:42:34 -0500

    add github link

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,4 +1,4 @@
-import { Settings } from "lucide-react"
+import { CodeXml, Settings } from "lucide-react"
 
 export default function NavigationBar({ onSettingsOpen }: {
   onSettingsOpen: () => void
@@ -7,7 +7,11 @@ export default function NavigationBar({ onSettingsOpen }: {
     <nav className="z-50 top-0 sticky py-3 px-6 bg-neutral-50 border-neutral-300 border-b flex items-center justify-between">
       <h1 className="font-display font-semibold text-5xl">cinch</h1>
 
-      <div>
+      <div className="flex items-center gap-2">
+        <a target="__window" href="https://github.com/writepoems/cinch" className="group p-2 cursor-pointer rounded-xl">
+          <CodeXml className="group-hover:animate-pulse" />
+        </a>
+
         <button className="group p-2 cursor-pointer rounded-xl" onClick={onSettingsOpen}>
           <Settings className="group-hover:rotate-180 duration-500" />
         </button>
