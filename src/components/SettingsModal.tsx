cfba21bc0c5dbb553595f443300be25e8a4d70d7
writepoems <272058035+writepoems@users.noreply.github.com>
2026-04-24 11:37:20 -0500

    add data wiping in settings

--- src/components/SettingsModal.tsx
+++ src/components/SettingsModal.tsx
@@ -1,4 +1,4 @@
-import { X } from "lucide-react";
+import { Trash2, X } from "lucide-react";
 
 export default function SettingsModal({ open, close }: {
   open: boolean
@@ -7,7 +7,7 @@ export default function SettingsModal({ open, close }: {
   return (
     <div id="dialog-container" className={open ? "" : "hidden"}>
       <dialog open className="z-100 h-2/3 w-1/3 relative border border-neutral-300 bg-neutral-50 shadow-lg rounded-xl p-4">
-        <div className="flex items-center justify-between">
+        <div className="flex items-center justify-between mb-2">
           <h3 className="font-bold font-display text-4xl">settings</h3>
 
           <button className="cursor-pointer" onClick={close}>
@@ -15,7 +15,13 @@ export default function SettingsModal({ open, close }: {
           </button>
         </div>
 
-        <p>Coming soon</p>
+        <button onClick={() => {
+          localStorage.clear()
+          location.reload()
+        }} className="btn bg-red-800 text-white rounded-xl">
+          <Trash2 size={18} />
+          Wipe Data
+        </button>
       </dialog>
     </div>
   )
