5e66e5d694864dd65570569a78489cec139f743b
writepoems <272058035+writepoems@users.noreply.github.com>
2026-04-28 14:41:15 -0500

    allow marking by clicking anywhere on todo item

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -17,16 +17,25 @@ export default function Todo({ label, done, onMarked, onDelete }: {
   onDelete: () => void
 }) {
   return (
-    <div className="group hover:bg-neutral-200/75 duration-200 p-1.5 rounded-xl flex items-center justify-between">
+    <div onClick={onMarked} className="group cursor-pointer hover:bg-neutral-200/75 duration-200 p-1.5 rounded-xl flex items-center justify-between">
       <div className="flex items-center gap-2">
-        <button onClick={onMarked}>
+        <button onClick={(e) => {
+          e.stopPropagation()
+          onMarked()
+        }}>
           <TodoCheck done={done} />
         </button>
 
         {done ? <s>{label}</s> : <p>{label}</p>}
       </div>
 
-      <button className="group-hover:opacity-100 duration-200 opacity-50 cursor-pointer" onClick={onDelete}>
+      <button 
+        className="group-hover:opacity-100 duration-200 opacity-50 cursor-pointer" 
+        onClick={(e) => {
+          e.stopPropagation()
+          onDelete()
+        }}
+      >
         <Trash2 size={18} className="text-red-500" />
       </button>
     </div>
