1b60362ae9f6eed7b2ce5d5cc62aa36753d5cd96
writepoems <272058035+writepoems@users.noreply.github.com>
2026-05-02 15:21:39 -0500

    polish modal open/close animation

--- src/components/SettingsModal.tsx
+++ src/components/SettingsModal.tsx
@@ -30,12 +30,20 @@ function SettingsModalContent({ close }: {
 }) {
   return (
     <motion.div 
-      initial={{ opacity: 0, transition: { duration: 0.25 } }}
-      animate={{ opacity: 1, transition: { duration: 0.25 } }}
-      exit={{ opacity: 0, transition: { duration: 0.25 } }}
+      transition={{ duration: 0.25 }}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
       id="dialog-container"
     >
-      <dialog open className="z-100 h-3/5 w-4/5 lg:h-2/3 lg:w-1/3 relative border border-neutral-300 bg-neutral-50 shadow-lg rounded-xl p-4">
+      <motion.dialog 
+        open 
+        transition={{ duration: 0.5, type: "spring" }}
+        initial={{ scale: 0, opacity: 0 }}
+        animate={{ scale: 1, opacity: 1 }}
+        exit={{ scale: 0, opacity: 0 }}
+        className="z-100 h-3/5 w-4/5 lg:h-2/3 lg:w-1/3 relative border border-neutral-300 bg-neutral-50 shadow-lg rounded-xl p-4"
+      >
         <div className="flex items-center justify-between mb-2">
           <h3 className="font-bold font-display text-4xl">settings</h3>
 
@@ -59,7 +67,7 @@ function SettingsModalContent({ close }: {
             </button>
           </div>
         </div>
-      </dialog>
+      </motion.dialog>
     </motion.div>
   )
 }
